9eaa50400bf057e60270c9ddb8458391a56b36a4
Anh <anhlevietexe@gmail.com>
2021-06-25 23:48:32 +1000

    move files outside

--- src/games/tron/index.tsx
+++ src/games/tron/index.tsx
@@ -3,19 +3,32 @@ import { Network } from '../../controllers/network';
 import message from './messages';
 
 const PREFIX = 'come-play-tron-'
+const network = new Network(PREFIX)
 
 const PlayerList = ({ players }: {players: string[]}) => {
   return <div>Players: {players.join(', ')}</div>
 }
 
-const Tron = () => {
+const Connect = ({network}: {network: Network}) => {
   const inputRef = React.useRef<HTMLInputElement>(null)
-  const [players, setPlayers] = React.useState<string[]>([])
-  const network = new Network(PREFIX)
   network.onConnect(() => {
     const intro: message = { type: 'introduction', value: { name: network.id.replace(PREFIX, ''), timestamp: Date.now() }}
     network.send(intro)
   })
+  const handleOnClick = (e) => {
+    if (inputRef === null || inputRef.current === null) return
+    network.connect(`${PREFIX}${inputRef.current.value}`)
+  }
+  return <>
+    <p>Your ID is: <b>{network.id.replace(PREFIX, '')}</b></p>
+    <label>Connect with: </label>
+    <input ref={inputRef}></input>
+    <button onClick={handleOnClick}>Connect</button>
+  </>
+}
+
+const Tron = () => {
+  const [players, setPlayers] = React.useState<string[]>([])
   network.receive((msg: message) => {
     console.log(msg)
     switch (msg.type) {
@@ -30,16 +43,9 @@ const Tron = () => {
         break;
     }
   })
-  const handleOnClick = (e) => {
-    if (inputRef === null || inputRef.current === null) return
-    network.connect(`${PREFIX}${inputRef.current.value}`)
-  }
-
+  
   return <>
-    <p>Your ID is: <b>{network.id.replace(PREFIX, '')}</b></p>
-    <label>Connect with: </label>
-    <input ref={inputRef}></input>
-    <button onClick={handleOnClick}>Connect</button>
+    <Connect network={network}/>
     <PlayerList players={players}/>
   </>
 };
