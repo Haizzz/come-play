e6e6be131cc80a3820d754552b8297bccb248a60
Anh <anhlevietexe@gmail.com>
2021-06-26 00:10:53 +1000

    regen yarn lock and format

--- src/games/tron/index.tsx
+++ src/games/tron/index.tsx
@@ -1,62 +1,62 @@
-import * as React from "react";
-import { Network } from "../../controllers/network";
-import message from "./messages";
+import * as React from 'react'
+import { Network } from '../../controllers/network'
+import message from './messages'
 
-const PREFIX = "come-play-tron-";
-const network = new Network(PREFIX);
+const PREFIX = 'come-play-tron-'
+const network = new Network(PREFIX)
 
 const PlayerList = ({ players }: { players: string[] }) => {
-  return <div>Players: {players.join(", ")}</div>;
-};
+  return <div>Players: {players.join(', ')}</div>
+}
 
 const Connect = ({ network }: { network: Network }) => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null)
   network.onConnect(() => {
     const intro: message = {
-      type: "introduction",
-      value: { name: network.id.replace(PREFIX, ""), timestamp: Date.now() },
-    };
-    network.send(intro);
-  });
-  const handleOnClick = (e) => {
-    if (inputRef === null || inputRef.current === null) return;
-    network.connect(`${PREFIX}${inputRef.current.value}`);
-  };
+      type: 'introduction',
+      value: { name: network.id.replace(PREFIX, ''), timestamp: Date.now() },
+    }
+    network.send(intro)
+  })
+  const handleOnClick = () => {
+    if (inputRef === null || inputRef.current === null) return
+    network.connect(`${PREFIX}${inputRef.current.value}`)
+  }
   return (
     <>
       <p>
-        Your ID is: <b>{network.id.replace(PREFIX, "")}</b>
+        Your ID is: <b>{network.id.replace(PREFIX, '')}</b>
       </p>
       <label>Connect with: </label>
       <input ref={inputRef}></input>
       <button onClick={handleOnClick}>Connect</button>
     </>
-  );
-};
+  )
+}
 
 const Tron = () => {
-  const [players, setPlayers] = React.useState<string[]>([]);
+  const [players, setPlayers] = React.useState<string[]>([])
   network.receive((msg: message) => {
-    console.log(msg);
+    console.log(msg)
     switch (msg.type) {
-      case "introduction":
-        console.log(`introduction by ${msg.value.name}`);
-        console.log(`ping: ${Date.now() - msg.value.timestamp}`);
-        setPlayers([...players, msg.value.name]);
-        break;
+      case 'introduction':
+        console.log(`introduction by ${msg.value.name}`)
+        console.log(`ping: ${Date.now() - msg.value.timestamp}`)
+        setPlayers([...players, msg.value.name])
+        break
 
       default:
-        console.log("message could not be parsed");
-        break;
+        console.log('message could not be parsed')
+        break
     }
-  });
+  })
 
   return (
     <>
       <Connect network={network} />
       <PlayerList players={players} />
     </>
-  );
-};
+  )
+}
 
-export default Tron;
+export default Tron
